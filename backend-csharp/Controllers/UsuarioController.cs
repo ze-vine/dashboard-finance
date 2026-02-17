c76c3f49f7da2f13e723559e916601fd3d4dcb03
José Vine <josevine729@gmail.com>
2026-02-16 22:25:22 -0300

    refactor: refatora a classe UsuarioController e implementa tratamento de exceções

--- backend-csharp/Controllers/UsuarioController.cs
+++ backend-csharp/Controllers/UsuarioController.cs
@@ -7,17 +7,30 @@ namespace backend_csharp.Controllers;
 [Route("api/usuarios")]
 public class UsuarioController : ControllerBase
 {
-    private readonly IUsuarioRepositorio repositorio;
+    private readonly IUsuarioRepositorio _repositorio;
 
     public UsuarioController(IUsuarioRepositorio repositorio)
     {
-        this.repositorio = repositorio;
+        _repositorio = repositorio;
     }
 
     [HttpGet]
-    public async Task<IActionResult> listarUsuarios()
+    public async Task<IActionResult> ListarUsuarios()
     {
-        var usuarios = await repositorio.ListarTodos();
-        return Ok(usuarios);
+        try
+        {
+            var usuarios = await _repositorio.ListarUsuarios();
+
+            if (!usuarios.Any())
+            {
+                return NotFound("Nenhum usuário foi cadastrado até o momento");
+            }
+
+            return Ok(usuarios);
+
+        } catch (Exception ex)
+        {
+            return StatusCode(500, "Ocorreu um erro inesperado");
+        }
     }
 }
